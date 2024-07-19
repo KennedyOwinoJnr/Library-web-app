1d4c2eec2fbb191d9691b7b81b203b90f46a943e
Kennedy Odhiambo <kennedyowino456@gmail.com>
2024-07-19 21:39:21 +0300

    corrections on settings file

--- LibraryApp/settings.py
+++ LibraryApp/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "TestProject.urls"
+ROOT_URLCONF = "LibraryApp.urls"
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "TestProject.wsgi.application"
+WSGI_APPLICATION = "LibraryApp.wsgi.application"
 
 
 # Database
