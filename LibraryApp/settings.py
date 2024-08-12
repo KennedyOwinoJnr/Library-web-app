10240c3d18cdee15d7b695d7340ff9a65e590b9f
Kennedy Odhiambo <kennedyowino456@gmail.com>
2024-08-12 16:31:33 +0300

    modify static file config

--- LibraryApp/settings.py
+++ LibraryApp/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
